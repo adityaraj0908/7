c4fa31513152308145d5ac3e34a2a0d2034c3858
Aditya Raj <130920748+adityaraj0908@users.noreply.github.com>
2024-07-30 06:46:17 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,86 +1,73 @@
 #include <stdio.h> 
-#include <stdbool.h> 
-#define MAX_PARTITIONS 10 
-#define MAX_PROCESSES 10111 
-struct Partition { 
-int id; 
-int size; 
-bool allocated; 
-}; 
-struct Process { 
-int id; 
-int size; 
-int partition_id; // ID of the allocated partition 
-}; 
-struct Partition partitions[MAX_PARTITIONS]; 
-struct Process processes[MAX_PROCESSES]; 
-int num_partitions = 0; 
-int num_processes = 0; 
-void initializePartitions() { 
-partitions[0].id = 1; 
-partitions[0].size = 50; 
-partitions[0].allocated = false; 
-partitions[1].id = 2; 
-partitions[1].size = 100; 
-partitions[1].allocated = false; 
-partitions[2].id = 3; 
-partitions[2].size = 500; 
-partitions[2].allocated = false; 
-num_partitions = 3; // Update number of partitions 
-} 
-void initializeProcesses() { 
-// Example processes initialization (you can modify this) 
-processes[0].id = 1; 
-processes[0].size = 80; 
-processes[1].id = 2; 
-processes[1].size = 150; 
-num_processes = 2; // Update number of processes 
-} 
- 
-void displayPartitions() { 
-    printf("Partitions:\n"); 
-    printf("ID\tSize\tAllocated\n"); 
-    for (int i = 0; i < num_partitions; i++) { 
-        printf("%d\t%d\t%s\n", partitions[i].id, partitions[i].size, 
-            partitions[i].allocated ? "Yes" : "No"); 
-    } 
-    printf("\n"); 
-} 
- 
-void displayProcesses() { 
-    printf("Processes:\n"); 
-    printf("ID\tSize\tPartition ID\n"); 
-    for (int i = 0; i < num_processes; i++) { 
-        printf("%d\t%d\t%d\n", processes[i].id, processes[i].size, 
-            processes[i].partition_id); 
-    } 
-    printf("\n"); 
-} 
- 
-void firstFit() { 
-    for (int i = 0; i < num_processes; i++) { 
-        for (int j = 0; j < num_partitions; j++) { 
-            if (!partitions[j].allocated && partitions[j].size >= processes[i].size) { 
-                // Allocate the partition to the process 
-                partitions[j].allocated = true; 
-                processes[i].partition_id = partitions[j].id; 
-                printf("Process %d allocated to Partition %d\n", processes[i].id, partitions[j].id); 
-                break; // Break out of inner loop once allocation is done 
+#include <string.h> 
+
+#define MAX_SIZE 20
+
+void lcsAlgo(char S1[], char S2[]) {
+    int i, j, m, n;
+    int LCS_table[MAX_SIZE][MAX_SIZE];
+    
+    m = strlen(S1); 
+    n = strlen(S2); 
+
+    // Initialize the LCS_table with zeros
+    for (i = 0; i <= m; i++) 
+        LCS_table[i][0] = 0; 
+    for (i = 0; i <= n; i++) 
+        LCS_table[0][i] = 0; 
+
+    // Fill the LCS_table based on the LCS algorithm
+    for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) { 
+            if (S1[i - 1] == S2[j - 1]) { 
+                LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1; 
+            } else if (LCS_table[i - 1][j] >= LCS_table[i][j - 1]) { 
+                LCS_table[i][j] = LCS_table[i - 1][j]; 
+            } else { 
+                LCS_table[i][j] = LCS_table[i][j - 1]; 
             } 
-        } 
-    } 
-} 
- 
+        }
+    }
+
+    // Length of the LCS
+    int index = LCS_table[m][n]; 
+    char lcs[index + 1]; 
+    lcs[index] = '\0'; 
+
+    // Trace the LCS_table to get the LCS string
+    i = m; 
+    j = n; 
+    while (i > 0 && j > 0) { 
+        if (S1[i - 1] == S2[j - 1]) { 
+            lcs[index - 1] = S1[i - 1]; 
+            i--; 
+            j--; 
+            index--; 
+        } else if (LCS_table[i - 1][j] > LCS_table[i][j - 1]) {
+            i--; 
+        } else {
+            j--; 
+        }
+    }
+
+    // Print the result
+    printf("S1: %s\nS2: %s\n", S1, S2); 
+    printf("LCS: %s\n", lcs); 
+}
+
 int main() { 
-    initializePartitions(); 
-    initializeProcesses(); 
-    printf("Initial State:\n"); 
-    displayPartitions(); 
-    displayProcesses(); 
-    firstFit(); 
-    printf("After Allocation:\n"); 
-    displayPartitions(); 
-    displayProcesses(); 
-   return 0; 
-} 
- 
+    char S1[MAX_SIZE], S2[MAX_SIZE]; 
+
+    printf("Enter the first string (S1): ");
+    fgets(S1, sizeof(S1), stdin);
+    // Remove newline character if present
+    S1[strcspn(S1, "\n")] = '\0';
+
+    printf("Enter the second string (S2): ");
+    fgets(S2, sizeof(S2), stdin);
+    // Remove newline character if present
+    S2[strcspn(S2, "\n")] = '\0';
+
+    lcsAlgo(S1, S2); 
+    return 0; 
+}
